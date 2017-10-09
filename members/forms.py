57d4ad5916d35c0d265a91d97ead7d4aeb922b61
Olzhas <o.arystanov@gmail.com>
2017-10-10 04:01:26 +0600

    templates modified

--- members/forms.py
+++ members/forms.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth import authenticate
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
 from .models import Member
 
 
@@ -13,6 +15,17 @@ class RegisterForm(forms.ModelForm):
                                widget=forms.PasswordInput)
     password2 = forms.CharField(max_length=32, label='Повтор пароля',
                                 widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_show_labels = False
+        self.helper.add_input(Submit('submit', 'Зарегистрироваться',
+                                     css_class='btn btn-block'))
+        layout = self.helper.layout = Layout()
+        for field_name, field in self.fields.items():
+            layout.append(Field(field_name, placeholder=field.label))
 
     def clean(self):
         cleaned_data = super(RegisterForm, self).clean()
@@ -30,7 +43,19 @@ class EditForm(forms.ModelForm):
 
 class SigninForm(forms.Form):
     email = forms.EmailField(label='Email')
-    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    password = forms.CharField(label='Пароль',
+                               max_length=32, widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(SigninForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_show_labels = False
+        self.helper.add_input(Submit('submit',
+                                     'Войти', css_class='btn btn-block'))
+        layout = self.helper.layout = Layout()
+        for field_name, field in self.fields.items():
+            layout.append(Field(field_name, placeholder=field.label))
 
     def clean(self):
         cleaned_data = super(SigninForm, self).clean()
--- partners/forms.py
+++ partners/forms.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth import authenticate
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
 
 
 class SigninForm(forms.Form):
     email = forms.EmailField(label='Email')
-    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    password = forms.CharField(label='Пароль',
+                               max_length=32, widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(SigninForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_show_labels = False
+        self.helper.add_input(Submit('submit', 'Войти',
+                                     css_class='btn btn-block'))
+        layout = self.helper.layout = Layout()
+        for field_name, field in self.fields.items():
+            layout.append(Field(field_name, placeholder=field.label))
 
     def clean(self):
         cleaned_data = super(SigninForm, self).clean()
